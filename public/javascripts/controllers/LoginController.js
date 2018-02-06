bdbcf89996d9ab90851da5a48d787530df43a9f5
Heather Kemp <heather-kemp@uiowa.edu>
2018-02-06 17:23:40 -0600

    Complete basic tutorial

---
+++ public/javascripts/controllers/LoginController.js
@@ -0,0 +1,16 @@
+angular.module('app')
+    .controller('LoginController', ['$scope', '$http', function($scope, $http) {
+        $scope.formData = {};
+        $scope.tagline = "Log In";
+    
+        $scope.login = function() {
+            $http.post('/api/login', $scope.formData)
+                .success(function(data) {
+                    $scope.user = data;
+                    console.log(data);
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        }
+    }]);
