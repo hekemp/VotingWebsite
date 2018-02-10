460e6cf30f14b7c39d8d07ea2ff3f5282a11a940
Junyan Yang <yangwu123789@gmail.com>
2018-02-09 18:35:46 -0600

    tutorial completed

---
+++ routes/frontend.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+/* route to handle all angular requests. */
+router.get('*', function(req, res) {
+  res.sendfile('./public/index.html');
+});
+module.exports = router;
