deb689c3a23c7a69528929dd2cca4d8ab53e6f81
6ru <1725366525@qq.com>
2018-02-12 13:12:30 -0600

    mlab

--- routes/frontend.js
+++ routes/frontend.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require("mongojs");
+var db = mongojs('mongodb://<stevenru>:<Rzy123456789>@ds233208.mlab.com:33208/votingsystem5800',['voting'] )
 /* route to handle all angular requests. */
 router.get('*', function(req, res) {
     res.sendfile('./public/index.html');
 });
+
 module.exports = router;
